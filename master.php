4b32437d03d42ffa247110eb0ef5edcc04c3441a
Reilley <reilleygray@gmail.com>
2019-04-17 08:49:40 +1000

    Changed placeholder to a heredoc

--- master.php
+++ master.php
@@ -3,7 +3,7 @@
 <?php require('_head.php'); ?>
 <body>
     <?php include('_header.php'); ?>
-    <div><?php echo($content); ?></div>
+    <div class='container'><?php echo($content); ?></div>
     <?php include('_footer.php'); ?>
 </body>
 </html>
