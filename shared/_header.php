a6966a4b32a5a8ba08eed60420dcf3fac654cc21
Reilley <reilleygray@gmail.com>
2019-04-17 12:47:06 +1000

    Made navbar brand link back to home

--- shared/_header.php
+++ shared/_header.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Coffee Buzz</a>
+        <a class="navbar-brand" href="../">Coffee Buzz</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="navbar-toggler-icon"></span>
         </button>
