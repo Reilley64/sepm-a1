6d037725779c803a1d262178ec93c5456a216a27
Reilley <reilleygray@gmail.com>
2019-05-06 13:18:24 +1000

    Fixes and optimizations for header and menu

--- web/routes.php
+++ web/routes.php
@@ -4,39 +4,39 @@ $app->get('/', function () use ($app) {
 });
 
 $app->get('/about', function () use ($app) {
-    return $app['twig']->render('about.html.twig');
+    return $app['twig']->render('about.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "About - "]);
 });
 
 $app->get('/cart', function () use ($app) {
     $database = new Database();
     $data = $database->selectAll("menu");
-    return $app['twig']->render('cart.html.twig', ["file" => $_SERVER['PHP_SELF'], "menu" => $data]);
+    return $app['twig']->render('cart.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Cart - ", "menu" => $data]);
 });
 
 $app->get('/coffee', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE type = 'COFFEE' ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Coffee Menu - ", "pageHeader" => "Coffee", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Coffee Menu - ", "menu" => $data]);
 });
 
 $app->get('/contact', function () use ($app) {
-    return $app['twig']->render('contact.html.twig');
+    return $app['twig']->render('contact.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Contact - "]);
 });
 
 $app->get('/food', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE type = 'FOOD' ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Food Menu - ", "pageHeader" => "Food", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Food Menu - ", "menu" => $data]);
 });
 
 $app->get('/menu', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Menu Menu - ", "pageHeader" => "Menu", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Menu Menu - ", "menu" => $data]);
 });
 
 $app->get('/tea', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE type = 'TEA' ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Tea - ", "pageHeader" => "Tea", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', ["file" => $_SERVER['PHP_SELF'], "title" => "Tea - ", "menu" => $data]);
 });
