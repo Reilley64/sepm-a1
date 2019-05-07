40cf87cdcab8568f849c219652e05e5d4a5f7f9c
Reilley <reilleygray@gmail.com>
2019-05-07 17:52:19 +1000

    Finished item pages

--- web/routes.php
+++ web/routes.php
@@ -4,45 +4,67 @@ $app->get('/', function () use ($app) {
 });
 
 $app->get('/about', function () use ($app) {
-    return $app['twig']->render('about.html.twig', ["title" => "About"]);
+    return $app['twig']->render('about.html.twig', [
+        "title" => "About",
+    ]);
 });
 
 $app->get('/cart', function () use ($app) {
     $database = new Database();
     $data = $database->selectAll("menu");
-    return $app['twig']->render('cart.html.twig', ["title" => "Cart", "menu" => $data]);
+    return $app['twig']->render('cart.html.twig', [
+        "title" => "Cart",
+        "menu" => $data,
+    ]);
 });
 
-$app->get('/coffee', function () use ($app) {
+$app->get('/menu/coffee', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE type = 'COFFEE' ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["title" => "Coffee Menu", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', [
+        "title" => "Coffee Menu",
+        "menu" => $data,
+    ]);
 });
 
 $app->get('/contact', function () use ($app) {
-    return $app['twig']->render('contact.html.twig', ["title" => "Contact"]);
+    return $app['twig']->render('contact.html.twig', [
+        "title" => "Contact",
+    ]);
 });
 
 $app->get('/item/{id}', function ($id) use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE id = '" . $id . "';");
-    return $app['twig']->render('item.html.twig', ["title" => ucfirst($data[0]->name), "item" => $data]);
+    return $app['twig']->render('item.html.twig', [
+        "title" => ucfirst($data[0]->name),
+        "item" => $data[0],
+    ]);
 });
 
-$app->get('/food', function () use ($app) {
+$app->get('/menu/food', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE type = 'FOOD' ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["title" => "Food Menu", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', [
+        "title" => "Food Menu",
+        "menu" => $data,
+    ]);
 });
 
 $app->get('/menu', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["title" => "Menu", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', [
+        "title" => "Menu",
+        "menu" => $data,
+    ]);
 });
 
-$app->get('/tea', function () use ($app) {
+$app->get('/menu/tea', function () use ($app) {
     $database = new Database();
     $data = $database->select("SELECT * FROM menu WHERE type = 'TEA' ORDER BY name;");
-    return $app['twig']->render('menu.html.twig', ["title" => "Tea Menu", "menu" => $data]);
+    return $app['twig']->render('menu.html.twig', [
+        "title" => "Tea Menu",
+        "menu" => $data,
+    ]);
 });
